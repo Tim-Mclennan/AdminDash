4f01e389ee4030ec6bb7486fd892c8413858ee42
Tim <timmymclennan@gmail.com>
2024-01-28 13:03:57 +1100

    Added utility functions to help with gathering user information. Passing user info to the customavatar component

--- src/components/layout/CurrentUser.tsx
+++ src/components/layout/CurrentUser.tsx
@@ -4,10 +4,8 @@ import { useGetIdentity } from "@refinedev/core"
 import type { User } from '@/graphql/schema.types'
 
 const CurrentUser = () => {
-  const { data: userData } = useGetIdentity<User>();
-
-  console.log(userData)
-
+  const { data: user } = useGetIdentity<User>();
+  
   return (
     <>
     <Popover
@@ -16,7 +14,12 @@ const CurrentUser = () => {
       overlayInnerStyle={{ padding: 0 }}
       overlayStyle={{ zIndex: 999 }}
     >
-      <CustomAvatar />
+    <CustomAvatar
+          name={user?.name}
+          src={user?.avatarUrl}
+          size="default"
+          style={{ cursor: "pointer" }}
+        />
     </Popover>
     </>
   )
