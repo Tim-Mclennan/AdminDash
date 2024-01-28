237cab62ac5248cbcb0a91618db3da45e932660a
Tim <timmymclennan@gmail.com>
2024-01-28 13:34:08 +1100

    Refactored Header component with better styling.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,33 @@
-import CurrentUser from "./CurrentUser"
+import React from "react";
+
+import { Layout, Space, theme } from "antd";
+
+import CurrentUser from "./CurrentUser";
+
+const { useToken } = theme;
 
 const Header = () => {
-  return (
-    <>
-      <CurrentUser />
-    </>
-  )
-}
+  const { token } = useToken();
 
-export default Header
+  const headerStyles: React.CSSProperties = {
+    backgroundColor: token.colorBgElevated,
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: "0px 24px",
+    height: "64px",
+    position: "sticky",
+    top: 0,
+    zIndex: 999,
+  };
+
+  return (
+    <Layout.Header style={headerStyles}>
+      <Space align="center" size="middle">
+        <CurrentUser />
+      </Space>
+    </Layout.Header>
+  );
+};
+
+export default Header;
