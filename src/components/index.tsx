6d58f268cd8191a9bcf98d354ff3848afa75fb72
Tim <timmymclennan@gmail.com>
2024-02-05 09:46:25 +1100

    Implemented components for company list page.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,5 +5,5 @@ export * from "./custom-avatar";
 export * from "./layout";
 export * from "./pagination-total";
 export * from "./select-option-with-avatar";
-// export * from "./tags";
+export * from "./tags";
 export * from "./text";
